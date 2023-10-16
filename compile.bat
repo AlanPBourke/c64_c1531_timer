6aec977aa68be167dcc7021ba37cae3af6e15d3b
Alan Bourke <alanpbourke@fastmail.fm>
2023-10-16 09:47:08 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 del %1.asm
 del %1.prg
 del %1.lbl
-rem "C:\Users\alanp\apps\millfork\millfork.exe" %1.mfk -o %1 -t c64 -s -G vice -fsource-in-asm
+
 "..\..\apps\millfork\millfork.exe" %1.mfk -o %1  -s -G vice -t .\c64_custom -O4 
 if exist %1.prg (
     x64sc -moncommands "C:\Users\alanp\dev\space_gits_64\%1.lbl" %1.prg 
